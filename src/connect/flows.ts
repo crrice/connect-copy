9a3f82cede3032f57e7c8cc60497918b57527617
Conor Rice <conorrice91@gmail.com>
2025-10-22 16:01:36 -0600

    Add flow and module listing utilities.
    Implements pagination-enabled utilities for listing and describing Amazon Connect resources:
    - `listContactFlows()`: List all contact flows in an instance
    - `listContactFlowModules()`: List all flow modules in an instance
    - `describeContactFlowModule()`: Get full module details including content

---
+++ src/connect/flows.ts
@@ -0,0 +1,68 @@
+
+import {
+  ConnectClient,
+  ListContactFlowsCommand,
+  ContactFlowSummary,
+  ListContactFlowModulesCommand,
+  ContactFlowModuleSummary,
+  DescribeContactFlowCommand,
+  ContactFlow,
+  DescribeContactFlowModuleCommand,
+  ContactFlowModule
+} from "@aws-sdk/client-connect";
+
+
+export async function listContactFlows(client: ConnectClient, instanceId: string): Promise<ContactFlowSummary[]> {
+  const flows: ContactFlowSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(new ListContactFlowsCommand({
+      InstanceId: instanceId,
+      NextToken: nextToken
+    }));
+
+    flows.push(...response.ContactFlowSummaryList ?? []);
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return flows;
+}
+
+
+export async function listContactFlowModules(client: ConnectClient, instanceId: string): Promise<ContactFlowModuleSummary[]> {
+  const modules: ContactFlowModuleSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(new ListContactFlowModulesCommand({
+      InstanceId: instanceId,
+      NextToken: nextToken
+    }));
+
+    modules.push(...response.ContactFlowModulesSummaryList ?? []);
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return modules;
+}
+
+
+export async function describeContactFlow(client: ConnectClient, instanceId: string, contactFlowId: string): Promise<ContactFlow> {
+  const response = await client.send(new DescribeContactFlowCommand({
+    InstanceId: instanceId,
+    ContactFlowId: contactFlowId
+  }));
+
+  return response.ContactFlow!;
+}
+
+
+export async function describeContactFlowModule(client: ConnectClient, instanceId: string, contactFlowModuleId: string): Promise<ContactFlowModule> {
+  const response = await client.send(new DescribeContactFlowModuleCommand({
+    InstanceId: instanceId,
+    ContactFlowModuleId: contactFlowModuleId
+  }));
+
+  return response.ContactFlowModule!;
+}
