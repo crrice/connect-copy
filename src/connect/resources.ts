f635231599f2d2c1763d77d70583747272b00d45
Conor Rice <conorrice91@gmail.com>
2025-10-22 16:43:12 -0600

    Add resource listing utilities for Connect supporting resources.
    Implements pagination-enabled list functions for all supporting resource types used in flow validation:
    
    - `listQueues()`
    - `listPrompts()`
    - `listRoutingProfiles()`
    - `listHoursOfOperations()`
    - `listQuickConnects()`
    - `listSecurityProfiles()`
    - `listUserHierarchyGroups()`
    - `listAgentStatuses()`

---
+++ src/connect/resources.ts
@@ -0,0 +1,207 @@
+
+import {
+  ListQueuesCommand,
+  ListPromptsCommand,
+  ListRoutingProfilesCommand,
+  ListHoursOfOperationsCommand,
+  ListQuickConnectsCommand,
+  ListSecurityProfilesCommand,
+  ListUserHierarchyGroupsCommand,
+  ListAgentStatusesCommand
+} from "@aws-sdk/client-connect";
+
+import type {
+  ConnectClient,
+  QueueSummary,
+  PromptSummary,
+  RoutingProfileSummary,
+  HoursOfOperationSummary,
+  QuickConnectSummary,
+  SecurityProfileSummary,
+  HierarchyGroupSummary,
+  AgentStatusSummary
+} from "@aws-sdk/client-connect";
+
+
+export async function listQueues(client: ConnectClient, instanceId: string): Promise<QueueSummary[]> {
+  const queues: QueueSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListQueuesCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.QueueSummaryList) {
+      queues.push(...response.QueueSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return queues;
+}
+
+
+export async function listPrompts(client: ConnectClient, instanceId: string): Promise<PromptSummary[]> {
+  const prompts: PromptSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListPromptsCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.PromptSummaryList) {
+      prompts.push(...response.PromptSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return prompts;
+}
+
+
+export async function listRoutingProfiles(client: ConnectClient, instanceId: string): Promise<RoutingProfileSummary[]> {
+  const routingProfiles: RoutingProfileSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListRoutingProfilesCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.RoutingProfileSummaryList) {
+      routingProfiles.push(...response.RoutingProfileSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return routingProfiles;
+}
+
+
+export async function listHoursOfOperations(client: ConnectClient, instanceId: string): Promise<HoursOfOperationSummary[]> {
+  const hoursOfOperations: HoursOfOperationSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListHoursOfOperationsCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.HoursOfOperationSummaryList) {
+      hoursOfOperations.push(...response.HoursOfOperationSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return hoursOfOperations;
+}
+
+
+export async function listQuickConnects(client: ConnectClient, instanceId: string): Promise<QuickConnectSummary[]> {
+  const quickConnects: QuickConnectSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListQuickConnectsCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.QuickConnectSummaryList) {
+      quickConnects.push(...response.QuickConnectSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return quickConnects;
+}
+
+
+export async function listSecurityProfiles(client: ConnectClient, instanceId: string): Promise<SecurityProfileSummary[]> {
+  const securityProfiles: SecurityProfileSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListSecurityProfilesCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.SecurityProfileSummaryList) {
+      securityProfiles.push(...response.SecurityProfileSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return securityProfiles;
+}
+
+
+export async function listUserHierarchyGroups(client: ConnectClient, instanceId: string): Promise<HierarchyGroupSummary[]> {
+  const hierarchyGroups: HierarchyGroupSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListUserHierarchyGroupsCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.UserHierarchyGroupSummaryList) {
+      hierarchyGroups.push(...response.UserHierarchyGroupSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return hierarchyGroups;
+}
+
+
+export async function listAgentStatuses(client: ConnectClient, instanceId: string): Promise<AgentStatusSummary[]> {
+  const agentStatuses: AgentStatusSummary[] = [];
+  let nextToken: string | undefined;
+
+  do {
+    const response = await client.send(
+      new ListAgentStatusesCommand({
+        InstanceId: instanceId,
+        NextToken: nextToken
+      })
+    );
+
+    if (response.AgentStatusSummaryList) {
+      agentStatuses.push(...response.AgentStatusSummaryList);
+    }
+
+    nextToken = response.NextToken;
+  } while (nextToken);
+
+  return agentStatuses;
+}
